984fb1a9cc8d0933fc2fecaedd0f86b3a1a9f1c8
este6an13 <diquintero@unal.edu.co>
2024-03-25 19:21:37 -0500

    reprebot/feat: add func to generate graph visualization
    - add `generate_knowledge_graph_output_files` to generate
      `dot`, `svg` and `gml` files of the knowledge graph
    - merge individual knowledge graphs into one big graph
    - add `graphviz`, `networkx` and `pydot` dependencies
    - start logic to merge nodes that mean the same (commented)

--- src/kg_generator/__init__.py
+++ src/kg_generator/__init__.py
@@ -5,23 +5,28 @@ from langchain.output_parsers import PydanticOutputParser
 from langchain_openai import ChatOpenAI
 from .types import KnowledgeGraph
 from llm_cache import LLMCache
+# from itertools import combinations
+from langchain_openai import OpenAIEmbeddings
+# from sklearn.metrics.pairwise import cosine_similarity
+# import numpy as np
 
 class KGGenerator:
     def __init__(self, model_name="gpt-3.5-turbo-0125"):
         self.api_key = os.environ["OPENAI_API_KEY"]
+        # self.embeddings = OpenAIEmbeddings(api_key=self.api_key)
         self.model_name = model_name
         self.model = self.setup_model()
         self.llm_cache = LLMCache()
         self.templates = {
             "KEY_TAKEAWAY":
                     """
-                        You are a a machine that extracts the key takeaway from a given paper abstract.\n
+                        You are a machine that extracts the key takeaway from a given paper abstract.\n
                         Paper Title: {title}
                         Paper Abstract: {abstract}
                     """,
             "KNOWLEDGE_GRAPH":
                     """
-                        You are a a machine that generates graphs by extracting concepts (nodes) and their relations (links) from a given paper summary.\n
+                        You are a machine that generates graphs by extracting concepts (nodes) and their relations (links) from a given paper summary.\n
                         Each link must have a word or short sentence describing the relationship.\n
                         The graph must be printed in JSON format. See the format instructions.\n
                         Format Instructions: {format_instructions}\n
@@ -88,7 +93,7 @@ class KGGenerator:
 
         return res
 
-    def get_knowledge_graph(self, article) -> str:
+    def get_article_knowledge_graph(self, article) -> str:
         article_id = article["id"]
         title = article["title"]
         abstract = article["abstract"]
@@ -100,3 +105,48 @@ class KGGenerator:
             )
             self.llm_cache.write(article_id, res)
         return res
+
+    def get_knowledge_graph(self, articles, threshold=0.95) -> str:
+        articles_knowledge_graphs = []
+        nodes = set()
+        links = []
+        for article in articles:
+            article_knowledge_graph = self.get_article_knowledge_graph(article)
+            print(article_knowledge_graph, "\n")
+            articles_knowledge_graphs.append(article_knowledge_graph)
+            for link in article_knowledge_graph["knowledge_graph"].links:
+                nodes.add(link.source)
+                nodes.add(link.target)
+                links.append(link)
+        merged_knowledge_graph = KnowledgeGraph(links = links)
+        # it takes too long: maybe using a vector database is faster
+        """
+        pairs = combinations(nodes, 2)
+        i = 0
+        tokens = 0
+        for pair in list(pairs):
+            x = np.array(self.embeddings.embed_query(pair[0])).reshape(1, -1)
+            y = np.array(self.embeddings.embed_query(pair[1])).reshape(1, -1)
+            tokens += len(pair[0].split())
+            tokens += len(pair[1].split())
+            i+=1
+            if i % 500 == 0:
+                print(i, tokens)
+            similarity = cosine_similarity(x, y)[0][0]
+            if similarity >= threshold and similarity < 1.0:
+                print(pair[0], pair[1], similarity)
+                # arbitrary selection
+                node_new_label = pair[0]
+                node_old_label = pair[1]
+                print(node_new_label, node_old_label)
+                for link in merged_knowledge_graph.links:
+                    if link.source == node_old_label:
+                        link.source = node_new_label
+                    if link.target == node_old_label:
+                        link.target = node_new_label
+        """
+        result = {
+            "articles_knowledge_graphs": articles_knowledge_graphs,
+            "merged_knowledge_graph": merged_knowledge_graph,
+        }
+        return result
